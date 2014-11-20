949081dbc8d4674bf31c4cfd40da063fedc149ea
Dima <dmitry.korolev@gmail.com>
2014-11-20 14:10:23 -0800

    Typo.

--- posix_socket.h
+++ posix_socket.h
@@ -18,7 +18,7 @@ const size_t kMaxQueuedConnections = 1024;
 const size_t kInitialHTTPHeaderSize = 1600;
 const double kHTTPHeaderSizeGrowthFactor = 1.95;
 
-// The separator is a blank line, via, http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
+// The separator is a blank line, via http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
 const char* kHTTPHeadBodySeparator = "\r\n\r\n";
 
 class GenericConnection {
